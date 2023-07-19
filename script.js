7442ed3cd393c648fd05d6a15146a6998386c64a
mahatosumit <91592750+mahatosumit@users.noreply.github.com>
2023-07-19 17:37:14 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll(".slider")
+var counter = 0;
+slides.each(
+    slider.foreach(
+        (slider, index) => {
+            slide.style.left = `${index * 100}%`
+        }
+    )
+)
